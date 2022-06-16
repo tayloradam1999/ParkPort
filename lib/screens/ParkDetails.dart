50c2bb7e2dd508dca2497008f3da58ab07eb4661
Adam Taylor <tayloradam1999@gmail.com>
2022-06-16 11:13:46 -0500

    Add holberton to park details carousel

--- lib/screens/ParkDetails.dart
+++ lib/screens/ParkDetails.dart
@@ -44,19 +44,26 @@ class _ParkDetailsState extends State<ParkDetails> {
         SingleChildScrollView(
             child: Column(
           children: [
-            ParkDetailsCard(),
+            ParkDetailsCard(
+                title: 'Chandler Park',
+                summary:
+                    '192 acres of wooded area with natural beauty and panoramic views of Tulsa and Sand Springs. There is a splash park, rock climbing, trails, baseball complex, playgrounds, restrooms, picnic shelters, 18-hole disc golf course, and community center.',
+                picture: 'assets/images/chandler.png',
+                address: '6500 W 21st St, Tulsa, OK 74107',
+                stamps: '10'),
           ],
         )),
         SingleChildScrollView(
             child: Column(
           children: [
-            ParkDetailsCard(),
-          ],
-        )),
-        SingleChildScrollView(
-            child: Column(
-          children: [
-            ParkDetailsCard(),
+            ParkDetailsCard(
+              title: 'Holberton Tulsa',
+              summary:
+                  'Holberton is a computer science school founded in Silicon Valley to address a gap in the education system for aspiring software engineers. Since Holberton’s coding school in Tulsa opened its doors, the world’s most innovative companies have begun to take notice.',
+              picture: 'assets/images/holberton.jpg',
+              address: '15 N Cheyenne Ave, Tulsa, OK 74103',
+              stamps: '10',
+            ),
           ],
         )),
       ]),
--- lib/widgets/park_details_card.dart
+++ lib/widgets/park_details_card.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ParkDetailsCard extends StatefulWidget {
-  ParkDetailsCard({Key? key}) : super(key: key);
+  late String title;
+  late String summary;
+  late String picture;
+  late String address;
+  late String stamps;
+
+  ParkDetailsCard({
+    Key? key,
+    required this.title,
+    required this.summary,
+    required this.picture,
+    required this.address,
+    required this.stamps,
+  }) : super(key: key);
 
   @override
   State<ParkDetailsCard> createState() => _ParkDetailsCardState();
@@ -21,19 +34,19 @@ class _ParkDetailsCardState extends State<ParkDetailsCard> {
           Stack(
             children: [
               Ink.image(
-                image: AssetImage('assets/images/chandler.png'),
+                image: AssetImage(widget.picture),
                 child: InkWell(
                   onTap: () {},
                 ),
                 height: 240,
                 fit: BoxFit.cover,
               ),
-              const Positioned(
+              Positioned(
                 bottom: 16,
                 right: 16,
                 left: 16,
                 child: Text(
-                  'Chandler Park',
+                  widget.title,
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
@@ -56,8 +69,8 @@ class _ParkDetailsCardState extends State<ParkDetailsCard> {
           ),
           Padding(
             padding: const EdgeInsets.all(16.0).copyWith(bottom: 0),
-            child: const Text(
-              '192 acres of wooded area with natural beauty and panoramic views of Tulsa and Sand Springs. There is a splash park, rock climbing, trails, baseball complex, playgrounds, restrooms, picnic shelters, 18-hole disc golf course, and community center.',
+            child: Text(
+              widget.summary,
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w400,
@@ -81,7 +94,7 @@ class _ParkDetailsCardState extends State<ParkDetailsCard> {
             children: [
               TextButton(
                 onPressed: () {},
-                child: const Text('6500 W 21st St, Tulsa, OK 74107',
+                child: Text(widget.address,
                     style: TextStyle(color: Color(0xFFe05e4a))),
               ),
             ],
@@ -102,7 +115,7 @@ class _ParkDetailsCardState extends State<ParkDetailsCard> {
             children: [
               TextButton(
                 onPressed: () {},
-                child: const Text('Stamp1',
+                child: Text(widget.stamps,
                     style: TextStyle(color: Color(0xFFe05e4a))),
               ),
             ],
